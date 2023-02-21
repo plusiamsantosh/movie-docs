93b9cdfba985a8f7483ae9ce68c297885be132ec
santoshanand <santoshanand.jogi@gmail.com>
2023-02-21 14:15:51 +0530

    added pnpm

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -17,6 +17,17 @@ export default {
         text: 'Database',
         items: [
           { text: 'Database Setup', link: '/db/database.md' },
+          { text: 'Database Migration', link: '/db/db-migration.md' },
+          { text: 'Add New Table', link: '/db/add-new-table.md' },
+        ]
+      },
+      {
+        text: 'Code',
+        items: [
+          { text: 'Add API', link: '/code/add-api.md' },
+          { text: 'New Service', link: '/code/new-service.md' },
+          { text: 'New Repository', link: '/code/new-repository.md' },
+          
         ]
       }
     ]
